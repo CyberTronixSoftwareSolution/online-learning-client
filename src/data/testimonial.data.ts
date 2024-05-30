dcd0158bbee7213e962e0bd27adfff22dd724ac2
lahiruvimukthi292 <lahiruvimukthi292@gmail.com>
2024-05-30 16:31:21 +0000

    add new home changes

--- src/data/testimonial.data.ts
+++ src/data/testimonial.data.ts
@@ -4,7 +4,7 @@ export const TestimonialData = [
     name: "Nimna Thiranjaya",
     rating: 5,
     review:
-      "As a busy mom, finding time to learn new skills was always a challenge. This online learning site made it possible for me to study at my own pace and on my schedule.",
+      "Expert in Data Science and AI, designed courses suitable for O-Level students. Courses: Introduction to Data Science, Basic Machine Learning, AI Fundamentals. Simplified, engaging content for beginners.",
     image:
       "https://res.cloudinary.com/dx1pvvqg7/image/upload/v1694811738/interview/profile_image/file_oxhsw0.jpg",
     date: "2023-09-01",
@@ -14,7 +14,7 @@ export const TestimonialData = [
     name: "Dimalka Heshan",
     rating: 4,
     review:
-      "I was skeptical about online learning, but this platform exceeded all my expectations. The interactive elements, like quizzes and forums, kept me engaged and motivated.",
+      "Business strategist with experience in teaching O-Level students. Courses: Basic Strategic Management, Introduction to Marketing, Leadership Principles, Entrepreneurship for Beginners. Practical and easy-to-understand lessons.",
     image:
       "https://res.cloudinary.com/dx1pvvqg7/image/upload/v1694811619/interview/profile_image/file_yyycis.jpg",
     date: "2023-09-01",
@@ -24,7 +24,7 @@ export const TestimonialData = [
     name: "Dinuka Tharindu",
     rating: 3,
     review:
-      "This site offers an incredible variety of courses that cater to all levels. Whether you're a beginner or looking to deepen your expertise, there's something for everyone. ",
+      "Specialist in Health and Wellness, tailored for O-Level students. Courses: Public Health Basics, Nutrition and Wellness, Mental Health Awareness, Introduction to Global Health. Comprehensive, accessible health education.",
     image:
       "https://res.cloudinary.com/dx1pvvqg7/image/upload/v1694788963/interview/profile_image/file_at9yxp.jpg",
     date: "2023-09-01",
